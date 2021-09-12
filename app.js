f31f028c2a3f3b06d9516dd331ce46a91f1afc82
Shivam Pandey <pandeyshivam1312@gmail.com>
2021-09-13 00:12:26 +0530

    text fetched and changed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var translateBtn = document.querySelector("#btn-translate");
 var inputArea = document.querySelector("#txt-area");
+var outputArea = document.querySelector(".output");
 
 translateBtn.addEventListener("click",clickHandler);
 
 function clickHandler() {
-    console.log("clicked");
     console.log(inputArea.value);
+    
+    outputArea.innerText = "🍌🍌🍌🍌 " + inputArea.value;
 }
