4ae6ebafbfcfe9c69d176f0b33225a6b3942e12e
Shivam Pandey <pandeyshivam1312@gmail.com>
2021-09-13 19:05:34 +0530

    prime day check

--- app.js
+++ app.js
@@ -2,39 +2,40 @@ var translateBtn = document.querySelector("#btn-translate");
 var inputArea = document.querySelector("#txt-area");
 var outputArea = document.querySelector(".output");
 
-var isLeapYear = false;
+var isPrimeNum = false;
 
 translateBtn.addEventListener("click",clickHandler);
 
 function clickHandler() {
-    let year = inputArea.value;
+    let date = inputArea.value;
+    let day = date.split("/");
 
-    if (year % 4 == 0) {
-
-        if (year % 100 ==0) {
-
-            if (year % 400 ==0) {
-                console.log("reached");
-
-                isLeapYear = true;
-            } else {
-                isLeapYear = false;
-            }
-
-        }
-            isLeapYear = true;
-
-
+    if (day[0] == 0 || day[0] == 1) {
+        isPrimeNum = false;
         
     } else {
-        isLeapYear = false;
+        let half = day[0]/2;
+        let flag =0;
+
+        for (let i = 2; i <= half; i++){
+            if (day[0] % i ==0) {
+                isPrimeNum = false;
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag ==0) {
+            isPrimeNum = true;
+        }
+        
     }
 
-    if (isLeapYear) {
-            outputArea.innerText = "It's a leap year " + inputArea.value;
+    if (isPrimeNum) {
+            outputArea.innerText = "It's a prime date " + inputArea.value;
 
     }else{
-        outputArea.innerText = "It's not a leap year " + inputArea.value;
+        outputArea.innerText = "It's not a prime date " + inputArea.value;
 
     }
     
