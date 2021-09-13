0293eadeb3f1ba4934d133ff356e66facd4786bb
Shivam Pandey <pandeyshivam1312@gmail.com>
2021-09-13 18:45:20 +0530

    checked leap year

--- app.js
+++ app.js
@@ -2,10 +2,40 @@ var translateBtn = document.querySelector("#btn-translate");
 var inputArea = document.querySelector("#txt-area");
 var outputArea = document.querySelector(".output");
 
+var isLeapYear = false;
+
 translateBtn.addEventListener("click",clickHandler);
 
 function clickHandler() {
-    console.log(inputArea.value);
+    let year = inputArea.value;
+
+    if (year % 4 == 0) {
+
+        if (year % 100 ==0) {
+
+            if (year % 400 ==0) {
+                console.log("reached");
+
+                isLeapYear = true;
+            } else {
+                isLeapYear = false;
+            }
+
+        }
+            isLeapYear = true;
+
+
+        
+    } else {
+        isLeapYear = false;
+    }
+
+    if (isLeapYear) {
+            outputArea.innerText = "It's a leap year " + inputArea.value;
+
+    }else{
+        outputArea.innerText = "It's not a leap year " + inputArea.value;
+
+    }
     
-    outputArea.innerText = "🍌🍌🍌🍌 " + inputArea.value;
 }
