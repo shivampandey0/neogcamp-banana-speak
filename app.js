3560b13d6970350943527dcce0d30fb831d1ba95
Shivam Pandey <pandeyshivam1312@gmail.com>
2021-09-15 20:10:45 +0530

    error handling

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var translateBtn = document.querySelector("#btn-translate");
 var inputArea = document.querySelector("#txt-area");
 var outputArea = document.querySelector(".output");
-
-
 translateBtn.addEventListener("click",clickHandler);
 
+var url = "https://api.funtranslations.com/translate/mindion.json"
+
 function clickHandler() {
+    var url = constructURL(inputArea.value);
+    fetch(url)
+    .then(response => response.json())
+    .then(data => outputArea.innerText = data.contents.translated)
+    .catch(errorHandler);
    
     
+}
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`
+}
+
+function errorHandler(error) {
+    outputArea.innerText = "Oops we hit the bananaa error\n " + error;
 }
